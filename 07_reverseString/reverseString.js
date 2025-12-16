8a54bdb3d7974c36bb461579d3e69dfaf6f9cdc3
Art <148421280+artant16@users.noreply.github.com>
2025-12-16 19:51:20 +1100

    Completed solution to exercise that passes all tests using the alternative approach of the provided solution.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringReverse = "";
+  
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringReverse += string[i];
+  }
+  
+  return stringReverse;
 };
 
 // Do not edit below this line
