cb16b0dbcce5fe882d3f4427b7d726916488c6e7
Art <148421280+artant16@users.noreply.github.com>
2025-12-18 21:57:17 +1100

    Spec file with .test removed to all for testing of all scenarios.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  if (((f - 32) / 1.8) % 1 === 0) {
+    return ((f - 32) / 1.8);
+  } 
+  return Number(((f - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  if (((c * 1.8 + 32)) % 1 === 0) {
+    return (c * 1.8 + 32);
+  }
+  return Number((c * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
