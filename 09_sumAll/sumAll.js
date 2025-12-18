bc08eeaae0e02926217d0fe9ef0feff4a322d7f5
Art <148421280+artant16@users.noreply.github.com>
2025-12-18 18:34:32 +1100

    Completed solution that passes all tests, although I took a different approach to the provided solution.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let difference = Math.abs(a - b);
+  let arrSum = [a];
 
+  if ((a <= 0 || b <= 0) || 
+      (a % 1 !== 0 || b % 1 !== 0) ||
+      (typeof a === "string" || typeof b === 'string')) {
+    return "ERROR";
+  }
+  else if (a <= b) {
+    for (let i = 1; i <= difference; i++) {
+      arrSum.push(a += 1);
+    }
+  } else {
+    for (let i = 1; i <= difference; i++) {
+      arrSum.push(a -= 1);
+    }
+  }
+
+  let result = arrSum.reduce((sum, curr) => sum + curr, 0);
+  return result;
 };
 
 // Do not edit below this line
