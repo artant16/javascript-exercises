e22d97a3985d44d37d588284f44f3ecf73edb82f
Art <148421280+artant16@users.noreply.github.com>
2026-03-20 09:15:46 +1100

    File updated with working solution.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+  if (n == 1) return 1;
 
+  let a = 0;
+  let b = 1;
+  let nextSum;
+
+  for (let i = 2; i <= n; i++) {
+    nextSum = a + b;
+    a = b;
+    b = nextSum;
+  } 
+  return b;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
